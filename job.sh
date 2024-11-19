17ab45c23ef46a5f1b5a7b040f871375949e81e4
qysgithubopen <139537508+qysgithubopen@users.noreply.github.com>
2024-11-19 17:26:15 +0800

    Update job.sh

--- job.sh
+++ job.sh
@@ -8,7 +8,7 @@ datapath=./datasets/BRATS2023_Training_none_npy/
 export CUDA_VISIBLE_DEVICES='1'
 
 
-resume=output_tmi2023
+resume=model_last.pth
 savepath=test_2023
 
 python train.py --batch_size=2 --datapath $datapath --savepath $savepath  --dataname $dataname  --resume $resume
